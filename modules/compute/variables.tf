794b2f0170acf9a0fa81db14f0ab2482c78dd3c7
Brijesh-12345 <110020974+Brijesh-12345@users.noreply.github.com>
2026-01-30 15:19:15 +0530

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -9,3 +9,33 @@ variable "asg_max" {
 variable "asg_desired" {
   type = number
 }
+variable "ami" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet IDs"
+  type        = list(string)
+}
+
+
+variable "sg_id" {
+  description = "Security group ID"
+  type        = string
+}
